feee570910bec3f70a21f3b3ad7016673d369956
muzijerrymuller <jerrymuller883@gmail.com>
2023-09-05 01:50:07 -0700

    6 abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,19 +6,11 @@
  * Return: value of the last digit
  */
 
-int print_last_digit(int n)
-{
-	int a;
-
-	if (n < 0)
-	n = -n;
-
-	a = n % 10;
-
-	if (a < 0)
-		a = -a;
-
-	_putchar(a + '0');
-
-	return (a);
-}
+int _abs(int n)
+	{
+		if (n >= 0)
+		{
+			return (n);
+		}
+		return (-n);
+	}
