c22be82757917a50985e8bc6a43a1e07bcd352ee
muzijerrymuller <jerrymuller883@gmail.com>
2023-10-23 05:41:37 -0700

    alphabetical orders

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
 	char alphabet;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
 
 	_putchar('\n');
