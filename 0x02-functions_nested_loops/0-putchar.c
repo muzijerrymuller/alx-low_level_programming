bed50f80d42c7bfd3092fe51d0ace254612a593a
muzijerrymuller <jerrymuller883@gmail.com>
2023-10-23 05:24:54 -0700

    putchar error update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * main - print _putchar
- * is written by mbah
+ *
+ *
  * Return: 0
  */
 
 
 int main(void)
 {
-_putchar('_putchar');
-_putchar('\n');
+	_putchar('_putchar');
+	_putchar('\n');
 return (0);
 }
