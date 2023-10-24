85d413115bbd6dc3c2f230956d00ef1dd4dc4034
muzijerrymuller <jerrymuller883@gmail.com>
2023-10-24 02:34:37 -0700

    last digit print

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
- * Return: value of the last digit
+ * print_last_digit -Last dif=git of num print
+ * @n: int to get the last digit from
+ * Return: Last digit value
  */
 
 int _abs(int n)
