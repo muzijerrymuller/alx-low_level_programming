655cbdc8076ef0777325fa886b78ab8a16705658
muzijerrymuller <jerrymuller883@gmail.com>
2023-12-13 04:19:41 -0800

    worrier

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,6 +3,8 @@
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
+
+
 void _puts(char *str)
 {
 	while (*str != '\0')
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 int _putchar(char c);
+void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
