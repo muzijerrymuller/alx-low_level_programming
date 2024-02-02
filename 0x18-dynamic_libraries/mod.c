6d1e8b9d74f8bc84da71c7fca2d5f799f8fdd42c
root <jerrymuller883@gmail.com>
2024-02-02 04:30:08 -0800

    test files

--- 0x18-dynamic_libraries/mod.c
+++ 0x18-dynamic_libraries/mod.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * add- mod of two int's.
+ * mod- mod of two int's.
  * @a- first int.
  * @b- second int.
  * Return: the sum.
  */
 
-int %(int a, int b)
+int mod(int a, int b)
 
 {
 	return (a % b);
