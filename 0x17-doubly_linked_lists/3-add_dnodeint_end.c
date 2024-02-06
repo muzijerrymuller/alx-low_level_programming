23da60a477855f433287589007ca7321581d6722
root <jerrymuller883@gmail.com>
2024-02-06 04:16:41 -0800

    add node int update

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,26 +9,26 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+new_node->n = n;
+new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
-    }
+if (*head == NULL)
+{
+new_node->prev = NULL;
+*head = new_node;
+return (new_node);
+}
 
-    dlistint_t *last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
+dlistint_t *last_node = *head;
+while (last_node->next != NULL)
+last_node = last_node->next;
 
-    last_node->next = new_node;
-    new_node->prev = last_node;
+last_node->next = new_node;
+new_node->prev = last_node;
 
-    return (new_node);
+return (new_node);
 }
